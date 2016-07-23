befbc0583d73b0381205d38bf43673b0739ceaee
ShevtsovSergey <shevtsov.sergey31@gmail.com>
2016-07-23 14:16:07 +0300

    Add Contact and Section enums

--- src/ru/javaresume/webapp/model/ContactType.java
+++ src/ru/javaresume/webapp/model/ContactType.java
@@ -4,10 +4,20 @@ package ru.javaresume.webapp.model;
  * Created by deadRabbit on 23.07.2016.
  */
 public enum ContactType {
-    PHONE,
-    MOBILE,
-    HOME_PHONE,
-    SKYPE,
-    MAIL,
-    HOME_PAGE
+    PHONE("Тел."),
+    MOBILE("Мобильный"),
+    HOME_PHONE("Домашний тел."),
+    SKYPE("Skype"),
+    MAIL("E-mail"),
+    HOME_PAGE("Домашняя страница");
+
+    private final String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
---
+++ src/ru/javaresume/webapp/model/SectionType.java
@@ -0,0 +1,23 @@
+package ru.javaresume.webapp.model;
+
+/**
+ * Created by deadRabbit on 23.07.2016.
+ */
+public enum SectionType {
+
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private final String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
